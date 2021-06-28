a9340ee4428898e6d283640f229acb15bb15a7c6
Indra-Pramana <80260627+Indra-Pramana@users.noreply.github.com>
2021-06-28 22:01:08 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,25 @@
+<div class="halaman">
+	<h2>KASUS HILANGNYA ANGGELINE</h2>
+    <img src="anggelin.jpg" width="400" height="300"
+	<p>Kepolisian Daerah Bali akhirnya menemukan bocah perempuan asal Bali, Angeline, 8 tahun, yang hilang sejak medio Mei 2015. Angeline ditemukan dalam keadaan tewas dan dikubur di rumah ibu angkatnya, Margareth. 
+Komisi Nasional Perlindungan Anak sempat mencurigai pelaku hilangnya bocah kelas III sekolah dasar itu justru keluarganya sendiri. Hingga akhirnya polisi menemukan Angeline tewas dengan luka dan dililit sebuah kain.
+berikut ini kronologi tragedi hilangnya Angeline.
+1. 16 Mei 2015
+Angeline terakhir terlihat di halaman rumahnya di Jalan Sedap Malam, Denpasar, Bali. Investigasi Komnas Anak menyatakan tetangga melihat pintu pagar rumah Angeline terkunci saat itu. "Artinya, hanya orang rumah yang tahu keberadaan terakhir Angeline. Dia tidak keluar," kata Arist.
+2. 17 Mei 2015
+Kakak angkat Angeline, Christina dan Ivon, mengumumkan hilangnya Angeline pada laman Facebook berjudul "Find Angeline-Bali's Missing Child". Mereka memasang sejumlah foto bocah yang senyumnya tampak ceria itu. Keduanya juga mengajak masyarakat ikut mencari Angeline. Masyarakat, dari artis hingga pejabat, geger ikut membantu pencarian bocah malang tersebut.
+3. 18 Mei 2015
+Tiga hari setelah menghilang, keluarga melapor ke Kepolisian Sektor Denpasar Timur. Polisi memeriksa sejumlah saksi, yaitu Margareth (ibu angkat Angeline), Antonius (pembantu sekaligus penjaga rumah), dan seorang penghuni kontrakan milik Margareth bernama Susianna.
+Polda Bali memperluas pencarian di seluruh perbatasan Bali, Banyuwangi, dan Nusa Tenggara Barat. Mereka juga memeriksa rumah Margareth tiga kali. Pemeriksaan pertama dan kedua selalu dihalangi pemilik rumah.
+4. 24 Mei 2015
+Ketua Komnas Perlindungan Anak Arist Merdeka Sirait mengunjungi rumah Margareth pada malam hari. Arist menengok kamar tidur Margareth yang juga sering dipakai Angeline. Menurut Arist, rumah itu tak layak huni karena acak-acakan, kotor, dan bau kotoran hewan. Margareth memelihara puluhan anjing dan ayam di rumahnya.
+Di kamar tidur, Arist mencium bau anyir yang berbeda dengan bau kotoran hewan. "Tidak ada seprei terpasang dan ruangannya bau anyir," ujar Arist. Kecurigaan itu segera dilaporkan kepada polisi.
+5. 5-6 Juni 2015
+Menteri Pemberdayaan Aparatur Negara dan Reformasi Birokrasi Yuddy Chrisnandi serta Menteri Pemberdayaan Perempuan dan Perlindungan Anak Yohana Yembise mengunjungi rumah Margareth dalam kesempatan berbeda. Namun kedatangan keduanya ditolak keluarga Angeline. 
+6. 9 Juni 2015
+Guru SD Negeri 12 Sanur Bali, tempat Angeline sekolah, menggelar sembahyang di depan Pura Penyimpangan Batu Bolong, di depan rumah Angeline. Persembahyangan digelar untuk meminta petunjuk paranormal. Mereka mengaku mendengar suara Angeline.
+7. 10 Juni 2015
+Polisi menemukan jasad Angeline di pekarangan rumah Margareth. Angeline ditemukan dikubur pada kedalaman setengah meter, dengan pakaian lengkap dan tangan memeluk boneka. Tubuhnya dililit seprei dan tali
+
+</p>
+</div>
